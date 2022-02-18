b530d3c9f26e11c43898b5a91ab2de2921798940
apachqq <work.tsyganov@mail.ru>
2022-02-19 02:02:13 +0300

    js-function

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ let list = document.querySelector('.todo-list');
 let input = document.querySelector('.todo-input');
 let form = document.querySelector('.todo-form');
 let priority = document.querySelector('.todo-priority');
+
+priority.onclick = function () {
+    if (priority.classList.contains('is-important')) {
+        priority.textContent = 'Обычная задача';
+    } else {
+        priority.textContent = 'Важная задача';
+    }
+    priority.classList.toggle('is-important');
+}
